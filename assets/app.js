91a9ed23871738ac0e1f8af80573af6b4707a026
fab-web-dev <97179540+fab-web-dev@users.noreply.github.com>
2022-04-26 16:30:32 +0200

    installation SASS,création controller home , création layout

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,37 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/styles.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+document.addEventListener("DOMContentLoaded", function() {
+    if (document.getElementById("mainButton")){
+        const mainButton = document.getElementById("mainButton");
+    mainButton.addEventListener("click", function() {
+        const mainVideo = document.getElementById("mainVideo");
+        mainVideo.classList.add("main-hide");
+        document.body.classList.remove("no-scroll");
+        scrollTo(0,0);
+    });
+    }
+
+    console.log(window.location.href.indexOf("no-intro"))
+    
+    if (window.location.href.indexOf("no-intro") !== -1){
+        const mainVideo = document.getElementById("mainVideo");
+        mainVideo.classList.add("no-transition");
+        mainVideo.classList.add("main-hide");
+        document.body.classList.remove("no-scroll");
+        scrollTo(0,0);
+    }
+
+    if (window.location.href.indexOf("contact") !== -1)
+    {
+        document.body.classList.add('contact-bg');
+    }
+    else {
+        document.body.classList.remove('contact-bg');
+    }
+});
